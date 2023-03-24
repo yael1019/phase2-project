498d46ec8eacf54bdbfb6fe5fb2c971bafdaed84
Yael Harosh <yaelharosh01@gmail.com>
2023-03-24 18:43:44 -0400

    creat a components folder, create a homepage and navbar, add content to homepage

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Homepage from './Homepage';
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <p>
+        -
+        <br />
+        -
+        <br />
+        -
+      </p>
+      <Navbar />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+        <button>Log In</button>
+        <h1>Exotic Garage</h1>
+        <img src='https://cdnb.artstation.com/p/assets/images/images/031/801/623/large/felippe-carballo-render1.jpg?1604620213' alt='car wheel' />
+    </div>
+  )
+}
+
+export default Homepage
