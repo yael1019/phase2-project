e7022903b7e2f0263e1be0b04420441d601d84ec
Yael Harosh <yaelharosh01@gmail.com>
2023-03-26 12:23:35 -0400

    make navbar open and close

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+const variants = {
+  open: {
+    opacity: 1,
+    x: 0
+  },
+  closed: {
+    opacity: 0,
+    x: '-100%'
+  }
+}
 
 function Navbar() {
+  const [clickBurger, setClickBurger] = useState(false);
+
   return (
     <div>
-      <div>
-      <input type="checkbox" />
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+    {/* HAMBURGER */}
+      <motion.button
+        className='hamburger'
+        onClick={() => setClickBurger(!clickBurger)}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        {
+          clickBurger
+          ?
+          <CloseIcon />
+          :
+          <MenuIcon />
+        }
+      </motion.button>
+
+      {/* NAV LINKS */}
+      <motion.nav
+        id='menu'
+        animate={clickBurger ? 'open' : 'closed'}
+        variants={variants}
+        transition={{ duration: 0.5 }}
+      >
+        <motion.div className='innerNav'>
+          <NavLink to='/' >Home</NavLink>
+
+        </motion.div>
+      </motion.nav>
+
     </div>
   )
 }
